27e7901453218a79182f7ac4a26075cd124cfec3
Hikaru Suido <hikaru.suido@gmail.com>
2017-05-15 16:39:58 +0900

    Implement dellt2

--- 2.ml
+++ 2.ml
@@ -11,10 +11,10 @@ let rec dellt a list =
 
 let rec dellt2 a list =
         if a>=0 then
-                match a with
-                        | 0 -> list
-                        | 1 -> list
-                        | _ -> let n::rest = list in let temp = n::[], dellt2 (a-1) rest
+                match (a, list) with
+                        |(0, list) -> list
+                        |(1, n2::r2) -> dellt2 0 r2             
+                        |(_, n1::r1) -> n1:: dellt2 (a-1) r1
         else
                 failwith "Exception: Failure"
 ;;
